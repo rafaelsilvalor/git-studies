55db267103f7ea581b7268ec98238d1df26a20eb
Rafale Lor <rafaelgslor@gmail.com>
2023-12-07 15:26:31 -0300

    Atualização hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 
-print("Hello World")
+
+name = input("Qual seu name? ")
+
+first_name, last_name = name.split(maxsplit=1)
+
+print(f"Hello, {first_name}!")
+print(f"Nome completo: {name}.")
+print(f"Primeiro nome: {first_name}.")
+print(f"Sobrenome: {last_name}")
